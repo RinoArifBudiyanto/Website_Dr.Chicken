8d9a5c9179d2da79f32bfedd07e0902b93ab7e2b
Rino Arif Budiyanto <rino.asuszen2@gmail.com>
2024-08-15 19:24:44 +0700

    First Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// const btn = document.getElementById("btn");
+
+// btn.addEventListener("click", function () {
+//   alert("Silahkan mampir ke outlet kami");
+// });
+
+const btn = document.querySelector(".btn");
